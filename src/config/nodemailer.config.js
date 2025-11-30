23a15954bb5eabd06be002a579bfd95dda021d6c
vikasFixl <vikasfixl@gmail.com>
2025-11-30 13:08:46 +0530

    fixed the mail issue

--- src/config/nodemailer.config.js
+++ src/config/nodemailer.config.js
@@ -1,45 +1,4 @@
-// import nodemailer from "nodemailer";
-// import { configDotenv } from "dotenv";
-// import logger from "./logger.js";
-// configDotenv();
 
-
-// export const transporter = nodemailer.createTransport({
-//   host: process.env.SMTP_HOST,
-//   port: process.env.SMTP_PORT,
-//   secure: process.env.SMTP_SECURE==="true", // true for 465, false for other ports
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass: process.env.SMTP_PASS,
-//   }, pool: true,          // keep 5 sockets alive
-//   maxConnections: 5,
-//   maxMessages: 100,
-//   rateDelta: 20000,    // 20 s window
-//   rateLimit: 5
-// });
-
-// // email.js
-// export function sendEmail({ to, subject, html, text }) {
-//   if (!to) {
-//     logger.error("sendEmailFast called without 'to'");
-//     return; // silent return, or queue a retry job
-//   }
-//   transporter.sendMail(
-//     {
-//       from: process.env.SMTP_FROM,
-//       to,
-//       subject,
-//       html,
-//       text,
-//     },
-//     (err, info) => {
-//       if (err) logger.error("Mail error", err);
-//       else logger.info("Mail sent", info.messageId);
-//     }
-//   );
-// }
-
-// resend.js
 import { Resend } from "resend";
 import { configDotenv } from "dotenv";
 import logger from "./logger.js";
@@ -62,7 +21,8 @@ export async function sendEmail({ to, subject, html, text }) {
       text,
     });
 
-    logger.info("Mail sent", response.id);
+    logger.info(`Mail sent successfully. ID: ${response.data.id}`);
+
     return response;
   } catch (error) {
     logger.error("Mail error", error);
